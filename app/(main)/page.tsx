11b5193b991e8fee9aabd826d58c3098c569bff3
Saul Lee <solleedata@gmail.com>
2024-06-13 23:33:09 +0900

    feat: Remove boilerplate code for layout and main page

---
+++ app/(main)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-screen gap-y-2'>
+      <h1 className='text-3xl font-bold rounded-md p-1 bg-slate-200 dark:bg-slate-700'>
+        라이더스 허브
+      </h1>
+      <h5>도로 위의 히어로, 대한민국 배달 라이더와 함께합니다</h5>
+
+      <div className='mt-5'>Kakao login</div>
+    </div>
+  );
+}
