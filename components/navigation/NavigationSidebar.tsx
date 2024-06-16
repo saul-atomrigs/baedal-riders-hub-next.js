ccc55310982c7b58ed4f9b4a28e10e88197398ff
Saul Lee <solleedata@gmail.com>
2024-06-16 13:30:03 +0900

    feat: Navigation Sidebar component

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,3 +1,37 @@
+import { signOut } from 'next-auth/react';
+import Link from 'next/link';
+
 export default function NavigationSidebar() {
-  return <div>NavigationSidebar</div>;
+  return (
+    <aside className='hidden sm:flex flex-col w-32'>
+      <h2 className='text-2xl font-bold m-3'>LOGO</h2>
+      <nav>
+        <ul className='text-sm mx-3 mb-3'>
+          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
+            <Link href='/' className='block w-full h-full'>
+              기 록
+            </Link>
+          </li>
+          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
+            <Link href='/user' className='block w-full h-full'>
+              수 입
+            </Link>
+          </li>
+          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
+            <Link href='/expense' className='block w-full h-full'>
+              지 출
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className='mb-3 mt-auto flex items-center flex-col gap-y-4'>
+        <button
+          onClick={() => signOut({ callbackUrl: '/' })}
+          className='hover:bh-gray-100'
+        >
+          Logout
+        </button>
+      </div>
+    </aside>
+  );
 }
