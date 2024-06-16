668f87427d9f65bdb0226ac1a8fe830362790943
Saul Lee <solleedata@gmail.com>
2024-06-16 21:29:12 +0900

    refactor: Sidebar menus

--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -6,24 +6,25 @@ export default function NavigationSidebar() {
   return (
     <aside className='hidden sm:flex flex-col w-32'>
       <h2 className='text-2xl font-bold m-3'>LOGO</h2>
-      <nav>
-        <ul className='text-sm mx-3 mb-3'>
-          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
-            <Link href='/' className='block w-full h-full'>
-              기 록
-            </Link>
-          </li>
-          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
-            <Link href='/user' className='block w-full h-full'>
-              수 입
-            </Link>
-          </li>
-          <li className='mb-1 p-1 w-full cursor-pointer rounded-md hover:bg-gray-100'>
-            <Link href='/expense' className='block w-full h-full'>
-              지 출
-            </Link>
-          </li>
-        </ul>
+      <nav className='text-sm mx-3 mb-3'>
+        <Link
+          href='/'
+          className='block w-full mb-1 p-1 cursor-pointer rounded-md hover:bg-gray-100 dark:hover:bg-gray-800'
+        >
+          기 록
+        </Link>
+        <Link
+          href='/user'
+          className='block w-full mb-1 p-1 cursor-pointer rounded-md hover:bg-gray-100 dark:hover:bg-gray-800'
+        >
+          수 입
+        </Link>
+        <Link
+          href='/expense'
+          className='block w-full  mb-1 p-1 cursor-pointer rounded-md hover:bg-gray-100 dark:hover:bg-gray-800'
+        >
+          지 출
+        </Link>
       </nav>
       <div className='mb-3 mt-auto flex items-center flex-col gap-y-4'>
         <ModeSelect />
