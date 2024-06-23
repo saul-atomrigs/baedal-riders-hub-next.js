0e890a527e5aa25c9ee1efe7cf0644be1521b5d1
Saul Lee <solleedata@gmail.com>
2024-06-23 11:16:51 +0900

    test: renders the corresponding icon to the current theme

---
+++ components/ModeSelect.spec.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ModeSelect from './ModeSelect';
+import { useTheme } from 'next-themes';
+
+// Mock the useTheme hook
+jest.mock('next-themes', () => ({
+  useTheme: jest.fn(),
+}));
+
+describe('ModeSelect', () => {
+  let setThemeMock: jest.Mock;
+
+  beforeEach(() => {
+    setThemeMock = jest.fn();
+    (useTheme as jest.Mock).mockReturnValue({
+      theme: 'light',
+      setTheme: setThemeMock,
+    });
+  });
+
+  it('renders the button with sun if theme is light', () => {
+    // Mock the useTheme hook:
+    setThemeMock.mockReturnValue('light');
+    render(<ModeSelect />);
+
+    const button = screen.getByRole('button', { name: /toggle theme/i });
+    expect(button).toBeInTheDocument();
+
+    const sunIcon = screen.getByLabelText('sun');
+
+    expect(sunIcon).toBeInTheDocument();
+  });
+
+  it('renders the button with moon if theme is dark', () => {
+    // Mock the useTheme hook:
+    setThemeMock.mockReturnValue('dark');
+    render(<ModeSelect />);
+
+    const button = screen.getByRole('button', { name: /toggle theme/i });
+    expect(button).toBeInTheDocument();
+
+    const moonIcon = screen.getByLabelText('moon');
+
+    expect(moonIcon).toBeInTheDocument();
+  });
+});
--- components/ModeSelect.tsx
+++ components/ModeSelect.tsx
@@ -19,19 +19,28 @@ export default function ModeSelect() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='ghost' size='icon'>
-          <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <Moon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <Sun
+            aria-label='sun'
+            className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
+          />
+          <Moon
+            aria-label='moon'
+            className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100'
+          />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
+        <DropdownMenuItem onClick={() => setTheme('light')} aria-label='light'>
+          light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
+        <DropdownMenuItem onClick={() => setTheme('dark')} aria-label='dark'>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
+        <DropdownMenuItem
+          onClick={() => setTheme('system')}
+          aria-label='system'
+        >
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
