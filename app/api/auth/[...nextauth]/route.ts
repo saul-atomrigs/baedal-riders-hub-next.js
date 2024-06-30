cbc8dc06371f73a09836f34b604e3d694109a2aa
Saul Lee <solleedata@gmail.com>
2024-06-30 12:24:10 +0900

    feat: integrate prisma and mongodb for user login with kakao

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,31 @@ const handler = NextAuth({
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const existingUser = await prisma.user.findUnique({
+          where: {
+            kakaoId: user.id,
+          },
+        });
+
+        if (!existingUser) {
+          const newUser = await prisma.user.create({
+            data: {
+              kakaoId: user.id,
+              name: user.name!,
+              bicycleType: '기타',
+            },
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
