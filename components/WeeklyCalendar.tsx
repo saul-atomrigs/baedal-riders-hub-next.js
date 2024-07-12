c81fd88bcbbe226a89927c785fa72dfc7e6d81b7
Saul Lee <solleedata@gmail.com>
2024-07-12 12:55:40 +0900

    feat: Add income bars with dummy data

--- components/WeeklyCalendar.tsx
+++ components/WeeklyCalendar.tsx
@@ -1,32 +1,41 @@
-import { format, startOfWeek, addDays, isToday, isSameDay } from 'date-fns';
+import { format, startOfWeek, addDays, isSameDay } from 'date-fns';
 
 type WeeklyCalendarProps = {
   label: string;
   currentDate: Date;
   onDateClick: (date: Date) => void;
+  barsData?: number[]; // Array of numbers representing the bar heights
 };
 
 export default function WeeklyCalendar({
   label,
   currentDate,
   onDateClick,
+  barsData = [20, 20, 20, 20, 50, 20, 30],
 }: WeeklyCalendarProps) {
   const daysOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
 
-  const start = startOfWeek(new Date());
+  const start = startOfWeek(new Date(), { weekStartsOn: 1 });
 
   return (
     <div className='flex flex-col gap-3'>
       <h3>{label}</h3>
       <div className='grid grid-cols-7 gap-4'>
         {daysOfWeek.map((day, index) => {
-          const date = addDays(start, index + 1);
+          const date = addDays(start, index);
           return (
             <div
               key={day}
               className='flex flex-col items-center cursor-pointer'
               onClick={() => onDateClick(date)}
             >
+              {/* Container for bar graph */}
+              <div className='flex flex-col-reverse items-center h-24 mb-2'>
+                <div
+                  className='bg-blue-500 w-4'
+                  style={{ height: `${barsData[index]}px` }}
+                />
+              </div>
               <div
                 className={`flex items-center justify-center w-8 h-8 rounded-md ${
                   isSameDay(date, currentDate)
