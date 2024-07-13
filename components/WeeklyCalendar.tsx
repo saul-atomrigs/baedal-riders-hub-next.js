f162f4eb6638a3e0168b40bd082d1c6172b4c934
Saul Lee <solleedata@gmail.com>
2024-07-13 16:07:14 +0900

    feat: daily income input UI

--- components/WeeklyCalendar.tsx
+++ components/WeeklyCalendar.tsx
@@ -1,5 +1,6 @@
 import { type IncomeType } from '@/app/api/incomes/route';
 import { format, startOfWeek, addDays, isSameDay } from 'date-fns';
+import { Input } from './ui/input';
 
 type WeeklyCalendarProps = {
   label: string;
@@ -15,6 +16,7 @@ export default function WeeklyCalendar({
   incomes,
 }: WeeklyCalendarProps) {
   const daysOfWeek = ['월', '화', '수', '목', '금', '토', '일'];
+  const TARGET = 500000;
 
   const start = startOfWeek(currentDate, { weekStartsOn: 1 });
 
@@ -34,7 +36,13 @@ export default function WeeklyCalendar({
 
   return (
     <div className='flex flex-col gap-3'>
-      <h3>{label}</h3>
+      <div className='flex justify-between'>
+        <h3>{label}</h3>
+        <div className=''>
+          <p>일일 목표</p>
+          <Input type='number' placeholder='원' />
+        </div>
+      </div>
       <div className='grid grid-cols-7 gap-4'>
         {daysOfWeek.map((day, index) => {
           const date = addDays(start, index);
@@ -48,7 +56,7 @@ export default function WeeklyCalendar({
               <div className='flex flex-col-reverse items-center h-24 mb-2'>
                 <div
                   className='bg-blue-500 w-4'
-                  style={{ height: `${barsData[index] / 1000}px` }}
+                  style={{ height: `${(barsData[index] * 100) / TARGET}px` }}
                 />
               </div>
               <div
