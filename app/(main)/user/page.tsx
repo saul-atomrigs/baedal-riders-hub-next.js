7d8de1d5c3dc2d3eb3aba156065f40e70dab7100
Saul Lee <solleedata@gmail.com>
2024-08-05 00:54:53 +0900

    feat: add support button in social page

--- app/(main)/user/page.tsx
+++ app/(main)/user/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import BuyMeACoffeeButton from '@/components/BuyMeACoffeeButton';
 
 export default function UserPage() {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen gap-y-2'>
-      <button onClick={() => signOut({ callbackUrl: '/' })}>Logout</button>
+      ⚙️ 열씨미 만들고 있어요... 곧 만나요!
+      <BuyMeACoffeeButton />
+      (개발자 커피 사주기)
     </div>
   );
 }
